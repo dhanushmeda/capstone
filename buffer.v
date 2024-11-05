5d31908481aa58c066371b6571f01427b55153a6
Pooja R <135737910+PoojaR07@users.noreply.github.com>
2024-11-05 11:10:41 +0530

    Update buffer.v

--- buffer.v
+++ buffer.v
@@ -5,18 +5,31 @@ module buffer (
     input wire shift,           // Signal to shift the data
     input [31:0] ip,
     input [15:0] port,
-    input wire [335:0] data_in, // 336-bit input data
-    output reg [7:0] data_out   // 8-bit output data
+    input wire [551:0] data_in, // 336-bit input data
+    output reg [7:0] data_out,   // 8-bit output data
+    input  wire         istart,
+    input  wire         ivalid,
+    output reg          iready,
+    //input  wire [ 7:0]  ibyte,
+    // image frame configuration output
+    output reg          ostart,
+    output wire [ 2:0]  colortype, // 0:gray   1:gray+A   2:RGB   3:RGBA   4:RGB-plte
+    output wire [13:0]  width,     // image width
+    output wire [31:0]  height,    // image height
+    // pixel output
+    output reg          ovalid,
+    output wire [ 7:0]  opixelr, opixelg, opixelb, opixela
+
     //output reg istart,
     //output reg ivalid
 );
 
-    reg [335:0] shift_reg; // 336-bit shift register
+    reg [551:0] shift_reg; // 336-bit shift register
     reg [2:0]  shift_count; // Counter to manage shifts
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            shift_reg <= 336'b0;
+            shift_reg <= 552'b0;
             data_out <= 8'b0;
             shift_count <= 3'b0;
         end else if (load) begin
@@ -32,14 +45,24 @@ module buffer (
         end
     end
 
+
+
+
 hard_png inst (
 	.clk(clk),
 	.rstn(rst),
 	.istart(istart),
 	.ivalid(ivalid),
+	.iready(iready),
 	.ibyte(data_out),
-	.ip(ip),
-
-	.port(port)	
+	.ostart(ostart),
+	.colortype(colortype),
+	.width(width),
+	.height(height),
+	.ovalid(ovalid),
+	.opixelr(opixelr),
+	.opixelg(opixelg),
+	.opixelb(opixelb),
+	.opixela(opixela)	
 );
 endmodule
